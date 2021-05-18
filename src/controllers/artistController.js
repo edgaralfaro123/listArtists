d773b4cecfb0b6a89d0d507a8da7f6063bd1f90f
edgar alfaro <soporte2@email.cloudnets.co>
2021-05-17 19:45:17 -0500

    refactor: refactorizo codigo

--- src/controllers/artistController.js
+++ src/controllers/artistController.js
@@ -1,16 +1,5 @@
-/* import api from '../helpers/constants'; */
 import Artist from '../models/artistModel.js'
 export async function getArtists () {
-   //console.log("api.URL_ARTISTS",api.URL_ARTISTS)
-    /* try { */
-        /* const response = await fetch('http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=spain&api_key=829751643419a7128b7ada50de590067&format=json', {
-            method: 'GET'
-        })
-        console.log("response",response.json())
-        console.log("response",response)
-        let json = response.json();
-        console.log("json",json._W)
-        return await ({status:response.status,data:response.json()})  */
         return new Promise((resolve, reject) => {
             const apiPath='http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=spain&api_key=829751643419a7128b7ada50de590067&format=json';
             fetch(apiPath)
@@ -33,11 +22,11 @@ export async function getArtists () {
                     }
                     return resolve(artistas)
                 },
-                (error) => reject(error)
+                (error) => {
+                    reject([]);
+                    console.log(error);
+                }
                 )
             }
         );
-    /* } catch (error) {
-        console.log(error)
-    } */
 }
